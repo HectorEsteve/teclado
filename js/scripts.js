2f8be7c0d80f94069d7c5e62cc6ed3dab63e2b45
HectorEsteve <hectorestevex2@gmail.com>
2024-11-20 17:07:41 +0100

    primero

---
+++ js/scripts.js
@@ -0,0 +1,24 @@
+// Espera a que el DOM esté completamente cargado
+document.addEventListener("DOMContentLoaded", function () {
+    // Seleccionar todos los botones y el input
+    const buttons = document.querySelectorAll(".button-custom");
+    const input = document.getElementById("output");
+
+    // Iterar sobre los botones para agregar funcionalidad
+    buttons.forEach((button) => {
+        button.addEventListener("click", function () {
+            const char = this.textContent.trim(); // Obtiene el texto del botón presionado
+
+            // Acciones según el botón presionado
+            if (char === "[ _ ]") {
+                input.value += " "; // Agregar un espacio
+            } else if (char === "DEL") {
+                input.value = input.value.slice(0, -1); // Borrar el último carácter
+            } else if (char === "D.ALL") {
+                input.value = ""; // Borrar todo el texto
+            } else {
+                input.value += char; // Agregar el carácter al final
+            }
+        });
+    });
+});
